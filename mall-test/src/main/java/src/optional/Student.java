90f8ca69d1ffe5da0021ef3f9f1801938064450b
zhanglianquan <1274096233@qq.com>
2020-12-04 14:45:04 +0800

    refactor: optional

--- mall-test/src/main/java/src/optional/Student.java
+++ mall-test/src/main/java/src/optional/Student.java
@@ -1,8 +1,14 @@
 package src.optional;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author zhanglianquan
  * @date 2020/12/4
  **/
+@Data
+@NoArgsConstructor
 public class Student {
+    private String name;
 }
--- mall-test/src/test/java/optional/TestOptional.java
+++ mall-test/src/test/java/optional/TestOptional.java
@@ -1,8 +1,38 @@
 package optional;
 
+import org.junit.Test;
+import src.multithread.Counter;
+import src.optional.Student;
+
+import java.util.Optional;
+
 /**
  * @author zhanglianquan
+ * 产考：https://blog.csdn.net/zjhred/article/details/84976734s
  * @date 2020/12/4
  **/
-public class testOptional {
+public class TestOptional {
+
+    @Test
+    public void testOptional(){
+//        Student std = null;
+        Student std = new Student();
+        std.setName("qq");
+//        Student stdNew= Optional.ofNullable(std).orElseGet(()->createStudent());
+//        Student stdNew2= Optional.ofNullable(std).orElse(createStudent());
+//        System.out.println(stdNew);
+//        System.out.println(stdNew2);
+
+        Student stdNew3= Optional.ofNullable(std).filter(s-> "zlq".equals(s.getName())).orElseGet(
+            ()->createStudent()
+        );
+        System.out.println(stdNew3);
+    }
+
+    private Student createStudent(){
+        Student std= new Student();
+        std.setName("zlq");
+        return std;
+    }
+
 }
