cf04ef798f595e86c81406ef295c9c13db35c33f
zhanglianquan <1274096233@qq.com>
2020-12-04 14:44:02 +0800

    refactor: optional

---
+++ mall-test/src/main/java/src/optional/Student.java
@@ -0,0 +1,8 @@
+package src.optional;
+
+/**
+ * @author zhanglianquan
+ * @date 2020/12/4
+ **/
+public class Student {
+}
---
+++ mall-test/src/test/java/optional/TestOptional.java
@@ -0,0 +1,8 @@
+package optional;
+
+/**
+ * @author zhanglianquan
+ * @date 2020/12/4
+ **/
+public class testOptional {
+}
