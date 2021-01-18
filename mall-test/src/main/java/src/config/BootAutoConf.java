3d4ccf49a8c0a596bfa9b55eeefbf95cb5526a13
zhanglianquan <1274096233@qq.com>
2021-01-18 16:31:39 +0800

    feat: update

--- mall-test/src/main/java/src/config/BootAutoConf.java
+++ mall-test/src/main/java/src/config/BootAutoConf.java
@@ -1,16 +1,18 @@
 package src.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * @author digen
  * Created on 2019/11/18
  **/
 @Configuration
-@ComponentScan(value = {"src.*", "src.annotation.spel"}
-//        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-//                value = {DataSourceAutoConfiguration.class})}
+@ComponentScan(value = {"src.*", "src.annotation.spel"},
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                value = {DataSourceAutoConfiguration.class})}
                 )
 public class BootAutoConf {
 }
--- mall-test/src/test/java/src/annotation/spel/TestSpel.java
+++ mall-test/src/test/java/src/annotation/spel/TestSpel.java
@@ -1,16 +1,20 @@
 package src.annotation.spel;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import src.BaseTest;
 
 /**
  * @author zhanglianquan
  * @date 2021/1/18
  **/
-public class TestSpel  extends BaseTest {
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = SpringBootApplication.class)
-//public class TestSpel   {
+//public class TestSpel  extends BaseTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootApplication.class)
+public class TestSpel   {
     @Test
     public void testSpel(){
         Carts carts= new Carts();
