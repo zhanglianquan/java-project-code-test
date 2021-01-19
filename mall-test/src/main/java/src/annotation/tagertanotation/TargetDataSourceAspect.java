f5dccf6f016c606e32eae1be9a5fb63697a93721
zhanglianquan <1274096233@qq.com>
2021-01-19 18:05:02 +0800

    feat: update

--- mall-test/src/main/java/src/annotation/tagertanotation/TargetDataSourceAspect.java
+++ mall-test/src/main/java/src/annotation/tagertanotation/TargetDataSourceAspect.java
@@ -1,16 +1,25 @@
 package src.annotation.tagertanotation;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.config.BeanExpressionContext;
 import org.springframework.beans.factory.config.BeanExpressionResolver;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.expression.StandardBeanExpressionResolver;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 import src.AppMain;
+
+import java.lang.reflect.Method;
 
 /**
  * @author zhanglianquan
@@ -58,4 +67,45 @@ public class TargetDataSourceAspect {
 
         System.out.println("使用了注解【com.ubuntuvim.sb.aop.TargetDataSource】的方法之前，先指定本方法。");
     }
+
+    @Pointcut("execution(* src.annotation..*.*(..))")
+    public void mobilePointCut() {
+    }
+    @Pointcut("@annotation(src.annotation.tagertanotation.TargetDataSource)")
+    public void viewPointCut() {
+    }
+
+//    @Around("mobilePointCut() && viewPointCut()")
+//    @Around("@annotation(targetDataSource)")
+//    public void doAround(ProceedingJoinPoint point, TargetDataSource targetDataSource){
+//        // 方法的参数值
+//        Object[] args = point.getArgs();
+//        //被代理的方法
+//        Method method = ((MethodSignature) point.getSignature()).getMethod();
+//
+//        //被代理的方法上的注解
+////        TargetDataSource targetDataSource = method.getAnnotation(TargetDataSource.class);
+//        String businessIdSpel = targetDataSource.value();
+//
+//        //获取被拦截方法参数名列表(使用Spring支持类库)
+//        LocalVariableTableParameterNameDiscoverer localVariableTable = new LocalVariableTableParameterNameDiscoverer();
+//        // 方法的参数名
+//        String[] paraNameArr = localVariableTable.getParameterNames(method);
+//        //使用SPEL进行key的解析
+//        ExpressionParser parser = new SpelExpressionParser();
+//        //SPEL上下文
+//        StandardEvaluationContext context = new StandardEvaluationContext();
+//        //将方法的参数名和参数值一一对应的放入SPEL上下文中
+//        for(int i=0;i<paraNameArr.length;i++) {
+//            context.setVariable(paraNameArr[i], args[i]);
+//        }
+//        String businessId = null;
+//        // 使用变量方式传入业务动态数据
+//        if(businessIdSpel.matches("^#.*.$")) {
+//            // 解析SpEL表达式获取结果
+//            businessId = parser.parseExpression(businessIdSpel).getValue(context, String.class);
+//        }
+//        System.out.println(businessId);
+//
+//    }
 }
